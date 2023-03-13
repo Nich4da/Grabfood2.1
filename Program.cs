934b08f56e4a14ee1005b56aaff296cc66d3ae6f
Nichada <marnichacha27@gmail.com>
2023-03-13 16:22:44 +0700

    grabfood2.1 only t-t

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿class Program {
+static void Main(string[] args){
+    int number = 1
+    Console.Write("input number: ");
+    Console.WriteLine("Monday");
+
+     int number = 2
+      Console.Write("input number: ");
+      Comsole.Writeline("Tuesday");
+
+     int number = 3
+      Console.Write("input number: ");
+      Comsole.Writeline("Wednesday");
+
+     int number = 4
+      Console.Write("input number: ");
+      Comsole.Writeline("Thursday");
+
+      int number = 5
+      Console.Write("input number: ");
+      Comsole.Writeline("Friday");
+
+      int number = 6
+      Console.Write("input number: ");
+      Comsole.Writeline("Saturday");
+      
+      int number = 7
+      Console.Write("input number: ");
+      Comsole.Writeline("Saturday");
+}
+}
